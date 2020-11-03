cdd9dbef4820e129875fcd6493f742aa11aa06db
Valeriia <valeriastanis@gmail.com>
2020-11-03 17:42:55 +0100

    css media changes

--- functions.php
+++ functions.php
@@ -14,8 +14,17 @@ function custom_add_google_fonts() {
  add_action( 'wp_enqueue_scripts', 'custom_add_google_fonts' );
 
 
+ add_action( 'after_setup_theme', 'yourtheme_setup' );
+ 
+ function yourtheme_setup() {
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+ }
 
-
-
+function mytheme_add_woocommerce_support() {
+	add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'mytheme_add_woocommerce_support' );
 
  
